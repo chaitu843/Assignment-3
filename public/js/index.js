4cfdffe35b10b6f01b16d458159501d2ad70e3cd
Sathi Reddy <satreddy@publicisgroupe.net>
2018-07-20 18:58:54 +0530

    With Single User Collection

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+require ("../../node_modules/jquery/dist/jquery.js");
+require ("../../node_modules/popper.js/dist/popper.js");
+require ("../../node_modules/bootstrap/dist/js/bootstrap.min.js");
+require ("../../node_modules/bootstrap/scss/bootstrap.scss"); 
+
+require('./base.js');
+require('../scss/base.scss');
